91e27370b0fed241ebe6325929d1339da30acb07
devo-ab <per.useravi0@gmail.com>
2025-07-07 22:49:22 +0600

    Headline data updfate dynamically

--- src/components/weather/WeatherBoard.jsx
+++ src/components/weather/WeatherBoard.jsx
@@ -6,19 +6,22 @@ import WeatherHeadline from "./WeatherHeadline";
 import { WeatherContext } from "../../context/WeatherContext";
 
 export default function WeatherBoard() {
-  const { weatherData } = useContext(WeatherContext);
-  console.log(weatherData)
+  const { weatherData, loading } = useContext(WeatherContext);
 
   return (
     <div className="container">
       <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-5xl mx-auto text-white">
-        <div className="grid md:grid-cols-2 gap-10 md:gap-6">
-          <AddToFavorite></AddToFavorite>
+        {loading.state ? (
+          <p>{loading.message}</p>
+        ) : (
+          <div className="grid md:grid-cols-2 gap-10 md:gap-6">
+            <AddToFavorite></AddToFavorite>
 
-          <WeatherHeadline></WeatherHeadline>
+            <WeatherHeadline></WeatherHeadline>
 
-          <WeatherCondition></WeatherCondition>
-        </div>
+            <WeatherCondition></WeatherCondition>
+          </div>
+        )}
       </div>
     </div>
   );
