67bc2041859ced2690ea9d9d37e72629fb2b9c69
devo-ab <per.useravi0@gmail.com>
2025-07-08 16:33:19 +0600

    updated file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Header from "./components/header/Header";
-import WeatherBoard from "./components/weather/WeatherBoard";
+
+import Page from "./Page";
 import { WeatherProvider, FavoriteProvider, LocationProvider } from "./provider";
 
 function App() {
@@ -7,16 +7,7 @@ function App() {
     <LocationProvider>
       <WeatherProvider>
         <FavoriteProvider>
-          <div className="h-screen bg-gradient-to-t from-gray-900 to-gray-700 grid place-items-center">
-            <div className="w-full">
-              <Header></Header>
-              <main>
-                <section>
-                  <WeatherBoard></WeatherBoard>
-                </section>
-              </main>
-            </div>
-          </div>
+          <Page></Page>
         </FavoriteProvider>
       </WeatherProvider>
     </LocationProvider>
---
+++ src/Page.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/header/Header";
+import WeatherBoard from "./components/weather/WeatherBoard";
+
+export default function Page() {
+  return (
+    <div className="h-screen bg-gradient-to-t from-gray-900 to-gray-700 grid place-items-center">
+      <div className="w-full">
+        <Header></Header>
+        <main>
+          <section>
+            <WeatherBoard></WeatherBoard>
+          </section>
+        </main>
+      </div>
+    </div>
+  );
+}
